a91d4f41ef7ca57dc64e009a2abbf71030575a8c
Antonio Mateos <principalemed@gmail.com>
2026-04-27 15:03:14 +0200

    Ruta y boton idioma

--- pialara/blueprints/main.py
+++ pialara/blueprints/main.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, session, redirect, request
 )
 from flask_login import login_required
 
@@ -8,3 +8,9 @@ bp = Blueprint('main', __name__)
 @login_required
 def index():
     return render_template('main/index.html')
+
+@bp.route('/set_language/<lang>')
+def set_language(lang):
+    if lang in ['es', 'en']:
+        session['lang'] = lang
+    return redirect(request.referrer or '/')
